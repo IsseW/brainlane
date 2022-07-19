0bcb6247c06ec25b4e5b7838b2395f1eab56729a
IsseW <isidornie@gmail.com>
2022-07-19 15:45:22 +0200

    better sending

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,11 @@
 #![feature(let_else)]
 use std::{
     env, fs,
-    io::{self, Read},
-    sync::atomic::{AtomicBool, AtomicU32, AtomicU8, Ordering},
+    sync::atomic::{AtomicU32, AtomicU64, AtomicU8, Ordering},
 };
 
 use array_init::array_init;
+use console::Term;
 
 enum IInstr {
     /// <
@@ -25,11 +25,11 @@ enum IInstr {
     /// ]
     EndLoop(usize),
     /// |
-    Wait(usize),
+    Wait(usize, u32),
     /// A..=Z
     Send(usize, usize),
     /// a..=z
-    Recieve(usize, usize),
+    Recieve(usize, usize, u32),
 }
 
 struct PInstr {
@@ -41,7 +41,12 @@ struct PInstr {
 
 impl PInstr {
     fn error(&self, kind: ErrorKind) -> Error {
-        Error { kind, c: self.c, x: self.x, y: self.y }
+        Error {
+            kind,
+            c: self.c,
+            x: self.x,
+            y: self.y,
+        }
     }
 }
 
@@ -63,11 +68,11 @@ enum Instr<'a> {
     /// ]
     EndLoop(usize),
     /// |
-    Wait(&'a Stop),
+    Wait(&'a Stop, u32),
     /// A..=Z
-    Send(&'a (AtomicBool, AtomicU8)),
+    Send(&'a Message),
     /// a..=z
-    Recieve(&'a (AtomicBool, AtomicU8)),
+    Recieve(&'a Message, u32),
 }
 
 #[derive(Default)]
@@ -90,13 +95,12 @@ impl<'a> Lane<'a> {
                 Instr::AddConst(num) => mem[ptr] = mem[ptr].wrapping_add(*num),
                 Instr::SubConst(num) => mem[ptr] = mem[ptr].wrapping_sub(*num),
                 Instr::Print => print!("{}", mem[ptr] as char),
-                Instr::Read => mem[ptr] = io::stdin().bytes().next().unwrap().unwrap(),
-                Instr::Wait(stop) => {
-                    stop.reach();
-                    while !stop.is_go() {}
+                Instr::Read => mem[ptr] = Term::stdout().read_char().unwrap() as u8,
+                Instr::Wait(stop, channel) => {
+                    stop.wait(*channel);
                 }
                 Instr::Last => {
-                    ptr = MEM.min(ptr.wrapping_sub(1));
+                    ptr = (MEM - 1).min(ptr.wrapping_sub(1));
                 }
                 Instr::Next => {
                     ptr = (ptr + 1) % MEM;
@@ -111,13 +115,11 @@ impl<'a> Lane<'a> {
                         new_line = Some(*start);
                     }
                 }
-                Instr::Send(send) => {
-                    send.1.store(mem[ptr], Ordering::Release);
-                    send.0.store(true, Ordering::Release);
+                Instr::Send(msg) => {
+                    msg.send(mem[ptr]);
                 }
-                Instr::Recieve(rec) => {
-                    while !rec.0.load(Ordering::Relaxed) { }
-                    mem[ptr] = rec.1.load(Ordering::Relaxed);
+                Instr::Recieve(msg, channel) => {
+                    mem[ptr] = msg.recieve(*channel);
                 }
             }
 
@@ -128,16 +130,61 @@ impl<'a> Lane<'a> {
 
 #[derive(Default)]
 struct Stop {
-    current: AtomicU32,
+    num: u32,
+    behind: AtomicU32,
+    in_front: AtomicU32,
+    lock: AtomicU64,
 }
 
 impl Stop {
-    fn reach(&self) {
-        self.current.fetch_sub(1, Ordering::Relaxed);
+    fn wait(&self, channel: u32) {
+        // Wait if there are is a lock already going on.
+        while self.lock.load(Ordering::Relaxed) & (1 << channel) == 1 {}
+        // Add one
+        self.behind.fetch_add(1, Ordering::Relaxed);
+
+        while self.behind.load(Ordering::Relaxed) < self.num {}
+
+        if self.in_front.fetch_add(1, Ordering::Relaxed) == self.num {
+            // This is the last in the lock, reset everything.
+            self.behind.store(0, Ordering::Relaxed);
+            self.in_front.store(0, Ordering::Relaxed);
+            self.lock.store(0, Ordering::Relaxed);
+        } else {
+            self.lock.fetch_or(1 << channel, Ordering::Relaxed);
+        }
     }
 
-    fn is_go(&self) -> bool {
-        return self.current.load(Ordering::Relaxed) == 0;
+    fn append(&mut self) -> u32 {
+        let c = self.num;
+        self.num += 1;
+        c
+    }
+}
+
+#[derive(Default)]
+struct Message {
+    has_sender: bool,
+    num_listeners: u32,
+    lock: AtomicU64,
+    msg: AtomicU8,
+}
+
+impl Message {
+    pub fn open_lock(&self) -> u64 {
+        (u64::MAX << self.num_listeners) >> self.num_listeners
+    }
+
+    pub fn send(&self, v: u8) {
+        while self.lock.load(Ordering::Relaxed) != self.open_lock() { }
+        self.msg.store(v, Ordering::Relaxed);
+        self.lock.store(0, Ordering::Relaxed);
+    }
+
+    pub fn recieve(&self, channel: u32) -> u8 {
+        while self.lock.load(Ordering::Relaxed) & (1 << channel) == 1 { }
+        self.lock.fetch_or(1 << channel, Ordering::Relaxed);
+        self.msg.load(Ordering::Relaxed)
     }
 }
 
@@ -146,7 +193,7 @@ const MAX_SENDABLE: usize = (b'Z' - b'A') as usize;
 struct Code {
     instructions: Vec<Vec<PInstr>>,
     stops: Vec<Stop>,
-    sends: Vec<[Option<(AtomicBool, AtomicU8)>; MAX_SENDABLE]>,
+    sends: Vec<[Message; MAX_SENDABLE]>,
 }
 
 impl Code {
@@ -161,55 +208,73 @@ impl Code {
             let mut instructions = Vec::<PInstr>::new();
             let mut loops = Vec::new();
 
+            let mut neg_last = false;
+
             for (x, c) in line.chars().enumerate() {
                 if x >= stops.len() {
                     stops.push(Stop::default());
-                    sends.push(array_init(|_| None));
+                    sends.push(array_init(|_| Message::default()));
                 }
-                let num_stops = &mut stops[x].current;
+                let stop = &mut stops[x];
                 let sends = &mut sends[x];
                 let error = |kind| Error { kind, c, x, y };
                 let err = |kind| Err(error(kind));
 
-                let instr = match c {
-                    '0' => IInstr::AddConst(10),
-                    '1'..='9' => IInstr::AddConst(c as u8 - b'0'),
-                    '-' => IInstr::SubConst(1),
-                    '+' => IInstr::AddConst(1),
-                    '.' => IInstr::Print,
-                    ',' => IInstr::Read,
-                    '<' => IInstr::Last,
-                    '>' => IInstr::Next,
-                    '[' => {
-                        loops.push(instructions.len());
-                        IInstr::StartLoop(0)
-                    }
-                    ']' => {
-                        let start = loops.pop().ok_or(error(ErrorKind::MismatchedLoop))?;
-                        let e = instructions.len();
-                        let IInstr::StartLoop(ref mut end) = instructions[start].kind else {
-                            // Can't be reached because values we push into loops will always be StartLoop.
-                            unreachable!()
-                        };
-                        *end = e;
-                        IInstr::EndLoop(start + 1)
-                    }
-                    '|' => {
-                        num_stops.fetch_add(1, Ordering::Relaxed);
-                        IInstr::Wait(x)
-                    }
-                    'A'..='Z' => {
-                        let b = (c as u8 - b'A') as usize;
-                        if sends[b].is_some() {
-                            return err(ErrorKind::DuplicateSend);
+                let neg = neg_last;
+                neg_last = false;
+
+                let instr = if neg {
+                    match c {
+                        '0' => IInstr::SubConst(10),
+                        '1'..='9' => IInstr::SubConst(c as u8 - b'0'),
+                        _ => {
+                            return err(ErrorKind::ExpectedNumber);
                         }
-                        sends[b].replace((AtomicBool::new(false), AtomicU8::new(0)));
-                        IInstr::Send(x, b)
                     }
-                    'a'..='z' => IInstr::Recieve(x, (c as u8 - b'a') as usize),
-                    ' ' => continue,
-                    _ => return err(ErrorKind::UnknownInstr),
+                } else {
+                    match c {
+                        '0' => IInstr::AddConst(10),
+                        '1'..='9' => IInstr::AddConst(c as u8 - b'0'),
+                        '-' => { neg_last = true; continue; },
+                        '.' => IInstr::Print,
+                        ',' => IInstr::Read,
+                        '<' => IInstr::Last,
+                        '>' => IInstr::Next,
+                        '[' => {
+                            loops.push(instructions.len());
+                            IInstr::StartLoop(0)
+                        }
+                        ']' => {
+                            let start = loops.pop().ok_or(error(ErrorKind::MismatchedLoop))?;
+                            let e = instructions.len();
+                            let IInstr::StartLoop(ref mut end) = instructions[start].kind else {
+                                // Can't be reached because values we push into loops will always be StartLoop.
+                                unreachable!()
+                            };
+                            *end = e;
+                            IInstr::EndLoop(start + 1)
+                        }
+                        '|' => IInstr::Wait(x, stop.append()),
+                        'A'..='Z' => {
+                            let b = (c as u8 - b'A') as usize;
+                            if sends[b].has_sender {
+                                return err(ErrorKind::DuplicateSend);
+                            }
+                            sends[b].has_sender = true;
+                            IInstr::Send(x, b)
+                        }
+                        'a'..='z' => {
+                            let b = (c as u8 - b'a') as usize;
+                            let channel = sends[b].num_listeners;
+                            sends[b].num_listeners += 1;
+                            IInstr::Recieve(x, (c as u8 - b'a') as usize, channel)
+                        }
+                        ' ' => continue,
+                        _ => return err(ErrorKind::UnknownInstr),
+                    }
                 };
+
+                
                 instructions.push(PInstr {
                     y,
                     x,
@@ -229,12 +294,37 @@ impl Code {
             instrs.push(instructions);
         }
 
+        for send in &mut sends {
+            for msg in send {
+                *msg.lock.get_mut() = msg.open_lock();
+            }
+        }
+
         for instructions in &instrs {
             for instr in instructions {
                 match instr.kind {
-                    IInstr::Recieve(i, b) => {
-                        sends[i][b].as_ref().ok_or(instr.error(ErrorKind::ReadWithoutSend))?;
-                    },
+                    IInstr::Recieve(i, b, _) => {
+                        sends[i][b]
+                            .has_sender
+                            .then_some(())
+                            .ok_or(instr.error(ErrorKind::ReadWithoutSend))?;
+                    }
+                    IInstr::Send(i, b) => {
+                        (sends[i][b].num_listeners > 0)
+                            .then_some(())
+                            .ok_or(instr.error(ErrorKind::SendWithoutRead))?;
+                        (sends[i][b].num_listeners <= 64)
+                            .then_some(())
+                            .ok_or(instr.error(ErrorKind::TooManyReaders))?;
+                    }
+                    IInstr::Wait(i, _) => {
+                        (stops[i].num > 1)
+                            .then_some(())
+                            .ok_or(instr.error(ErrorKind::SingleWait))?;
+                        (stops[i].num <= 64)
+                            .then_some(())
+                            .ok_or(instr.error(ErrorKind::TooManyWaits))?;
+                    }
                     _ => {}
                 }
             }
@@ -255,9 +345,14 @@ struct Program<'a> {
 #[derive(Debug)]
 enum ErrorKind {
     UnknownInstr,
+    ExpectedNumber,
     MismatchedLoop,
     DuplicateSend,
     ReadWithoutSend,
+    TooManyReaders,
+    SendWithoutRead,
+    SingleWait,
+    TooManyWaits,
 }
 
 #[allow(dead_code)]
@@ -287,9 +382,9 @@ impl<'a> Program<'a> {
                     IInstr::Read => Instr::Read,
                     IInstr::StartLoop(n) => Instr::StartLoop(n),
                     IInstr::EndLoop(n) => Instr::EndLoop(n),
-                    IInstr::Wait(n) => Instr::Wait(&code.stops[n]),
-                    IInstr::Send(i, b) => Instr::Send(code.sends[i][b].as_ref().unwrap()),
-                    IInstr::Recieve(i, b) => Instr::Recieve(code.sends[i][b].as_ref().unwrap()),
+                    IInstr::Wait(n, c) => Instr::Wait(&code.stops[n], c),
+                    IInstr::Send(i, b) => Instr::Send(&code.sends[i][b]),
+                    IInstr::Recieve(i, b, c) => Instr::Recieve(&code.sends[i][b], c),
                 })
                 .collect::<Vec<_>>();
 
